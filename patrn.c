f5bda0e387c58a17c6e53651891a9eef5f588379
Niketan Kumar <niketanku12@gmail.com>
2026-02-12 21:23:35 +0530

    patrn.c

---
+++ patrn.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+
+void computeLPSArray(char* pat, int m, int* lps) {
+    int len = 0;
+    lps[0] = 0;
+    int i = 1;
+
+    while (i < m) {
+        if (pat[i] == pat[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+void search(char* pat, char* txt) {
+    int m = strlen(pat);
+    int n = strlen(txt);
+
+    int lps[m];
+    computeLPSArray(pat, m, lps);
+
+    int i = 0; 
+    int j = 0; 
+
+    while (i < n) {
+        if (pat[j] == txt[i]) {
+            i++;
+            j++;
+        }
+
+        if (j == m) {
+            printf("Pattern found at index %d\n", i - j);
+            j = lps[j - 1];
+        }
+        else if (i < n && pat[j] != txt[i]) {
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i++;
+        }
+    }
+}
+
+int main() {
+    char txt[] = "AABAACAADAABAABA";
+    char pat[] = "AABA";
+    search(pat, txt);
+    return 0;
+}
+
+
+output:
+
+
+Pattern found at index 0
+Pattern found at index 9
+Pattern found at index 12
+
+
+=== Code Execution Successful ===
