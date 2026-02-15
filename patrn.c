9ef5720203e0ec7db35f8071e999e41b049b78a0
Niketan Kumar <niketanku12@gmail.com>
2026-02-15 21:06:27 +0530

    Update patrn.c

--- patrn.c
+++ patrn.c
@@ -68,3 +68,48 @@ Pattern found at index 12
 
 
 === Code Execution Successful ===
+#include <stdio.h>
+
+int main() {
+    int i, j, n = 5;
+    
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= n; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
+
+
+#include <stdio.h>
+
+int main() {
+    int i, j, n = 5;
+    
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    int i, j, n = 5;
+    
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
